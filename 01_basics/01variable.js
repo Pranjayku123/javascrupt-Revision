db794973ad117029cef1b215b760cbf2b180d94b
#DEVprk <109287745+Pranjayku123@users.noreply.github.com>
2025-02-10 17:34:06 +0000

    dataTypes

---
+++ 01_basics/01variable.js
@@ -0,0 +1,15 @@
+console.log("Pranjay Happy Now");
+const constentVar = 101;
+let userName = "Pranjay Kumar";
+var password = "prk@123";
+errorMessage = "Invalid"
+
+// constentVar = 123;
+userName = "golu";
+
+console.log(constentVar);
+console.log(userName);
+console.log(password);
+console.log(errorMessage);
+
+// prefare not to use var keyword in you regulor programming 
---
+++ 01_basics/02dataType.js
@@ -0,0 +1,19 @@
+"use strict";
+// treet all javascript code as per newer version 
+
+
+
+// number => 2 to the 53    //less then 18 digits
+//bigint
+//string=> ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+ // symbol => unique
+//object
+
+console.log(typeof "Pranjay");
+
+console.log(typeof null);
+ 
+
